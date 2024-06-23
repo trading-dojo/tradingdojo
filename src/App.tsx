db1857f37bced24b592d27eb654354cd410bda46
trading-dojo <webtests8484@gmail.com>
2024-06-23 03:01:54 -0700

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './components/Home';
